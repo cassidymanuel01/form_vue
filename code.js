f130d0044f04a1a972b2e74237399e984cc571e0
cassidymanuel01 <cassidymanuel.59@gmail.com>
2022-07-04 16:18:30 +0200

    https://github.com/cassidymanuel01/form_vue.git

--- code.js
+++ code.js
@@ -1,39 +1,38 @@
 const form = Vue.createApp({
     template: `
     <div id="form">
-    <form><br>
+    <form onsubmit="event.preventDefault()"><br>
     <label>Name:</label>
     <input type="text" placeholder="Enter your name... " v-model="name"/><br><br>
     <label>Surname:</label>
     <input type="text" placeholder="Enter your surname" v-model="surname"/><br><br>
     <label>Age:</label>
-    <input type="number" placaeholder="Enter your age" v-model="age"/><br><br>
+    <input type="number" v-model="age"/><br><br>
     <button v-on:click="display">Submit</button>
-    <p v-if="isVisiable">you are of age</p>
-    <p v-if="!isVisiable">you are not of age</p>
+    <p v-if="isVisiable && !underAge">you are of age</p>
+    <p v-if="isVisiable && underAge">you are not of age</p>
     </form>
     </div>
     `,
     data: function(){
         return {
-            name : '',
-            surname :'',
-            age : '',
-            isVisiable: true,
-            underAge: false,
-            ofAge: 18
+            name : "",
+            surname :"",
+            age : "",
+            isVisiable: false,
+            underAge: true,
         }
     },
     methods: {
-        display(e) {
-            e.preventDefault;
-            this.isVisiable = !this.isVisiable;
-            if (age>17){
-                underAge = false
-            }else if (age=18){
-                ofAge = true
-            }else{
-                underAge = true
+        display() {
+            if(!this.isVisiable){
+                this.isVisiable = !this.isVisiable;
+            }
+            if (this.age>17){
+                this.underAge = false
+            }
+            else{
+                this.underAge = true
             }
         }
     }
